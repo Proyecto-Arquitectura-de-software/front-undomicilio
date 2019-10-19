4a4ef0c3895316153044bc088fbc0f81e82e6246
Christian Alfonso <csalfonsol@unal.edu.co>
2019-10-19 16:59:53 -0500

    Se crea un servidor node temporal para ver la vista previa
    Posteriormente, como este app es solo el front, se podra elimintar node modules y algunas cosas mas

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ class App extends Component {
       <div>
         <Hello name={this.state.name} />
         <p>
-          Primer parrafo en ingles
+          Primer parrafo en espanol
         </p>
       </div>
     );
