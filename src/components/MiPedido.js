e4bba6ec9b7392d8034be49cd05e108154d16dd1
Christian Alfonso <csalfonsol@unal.edu.co>
2019-10-23 21:16:17 -0500

    Consulta a micro mejorado

--- src/components/MiPedido.js
+++ src/components/MiPedido.js
@@ -1,102 +1,122 @@
 import React, { Component } from 'react';
 import { productos } from '../productos.json';
-import { pedido } from '../pedido.json';
 
 class MiPedido extends Component {
     constructor() {
       super();
       this.state = {
-        productos,
-        pedido    
+        productos
       };
+      this.state.message = "Cargando";
+      this.state.envio = 1000;
     }
 
      // Se ejecuta cuando se carga el componente
-     componentDidMount() {
-      this.obtenerDatos();
+     componentDidMount() {      
+      this.obtenerDatos();    
     }
 
     async obtenerDatos(){
-
-      //const get = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-      const get = await fetch('http://localhost:3100/pedidos');
       
+      const get = await fetch('http://localhost:3100/pedidos/1');      
       const data = await get.json();
+      this.setState({"pedido" : data});
+      console.log("Los datos " + data);
+          
+    }
 
-      console.log(data);
+    calcularSubtotal() {
       
-      
-    } 
+      var resultado = 0;
+      const prod = this.state.productos;
+
+      for (var i = 0; i < prod.length; i++) {
+        resultado += parseInt(prod[i].precio);
+      }
+
+      return resultado;
+
+    }
   
     render() {      
       
-      const pedido = this.state.pedido[0];
+      
+      if (this.state.pedido){
 
-      const productos = this.state.productos.map((item, i) => {
-        return (
-          <div className="col-md-4" key={i}>
-            <div className="card mt-4">
-              <div className="card-title text-center">
-                <h3>{item.nombre}</h3>
-                <h4>
-                  <span className="badge badge-pill badge-warning ml-2">
-                    $ {item.precio}
-                  </span>
-                </h4>
-              </div>
-              <div className="card-footer">
-                <button
-                  className="btn btn-danger invisible"
-                  onClick={console.log('Para eliminar')}>
-                  Eliminar
-                </button>
-              </div>
-            </div>
-          </div>
-        )
-      });
-  
-      return (
-
-        <div>                    
-          <div>
-            <div className="ml-4 mt-3">
-              <h1 className="text-center">Estado de mi pedido</h1>
-
-              <p className="">Destino: <strong>{pedido.destino}</strong> </p>              
-              <p className="">Estado: <strong>{pedido.estado}</strong> </p>
-            </div>            
-          </div>          
-            
-            <nav className="navbar navbar-dark bg-dark">
-              <a className="navbar-brand" href="/">
-                Productos
-                <span className="badge badge-pill badge-light ml-2">
-                  {this.state.productos.length}
-                </span>
-              </a>
-            </nav>
-
-            <div className="container mb-4">                     
-              <div className="col-md-8">
-                  <div className="row">
-                    {productos}
+        const pedido = this.state.pedido[0];              
+        
+        const productos = this.state.productos.map((item, i) => {
+          return (            
+              <div className="col-md-4" key={i}>
+                <div className="card mt-4">
+                  <div className="card-title text-center">
+                    <h3>{item.nombre}</h3>
+                    <h4>
+                      <span className="badge badge-pill badge-warning ml-2">
+                        $ {item.precio}
+                      </span>
+                    </h4>
                   </div>
-                </div>              
-            </div>
-
-            <div>
-              <div className="ml-4">              
-                <p className="">Observaciones: <strong>{pedido.observaciones}</strong> </p>              
-                <p className="">Subtotal: <strong>$ {pedido.subtotal}</strong> </p>
-                <p className="">Envio: <strong>$ {pedido.envio}</strong> </p>
-
-                <h4 className="">Total: <strong>$ {parseInt(pedido.envio) + parseInt(pedido.subtotal)}</strong> </h4>
+                  <div className="card-footer">
+                    <button
+                      className="btn btn-danger invisible"
+                      onClick={console.log('Para eliminar')}>
+                      Eliminar
+                    </button>
+                  </div>
+                </div>
               </div>            
-            </div>
+          )
+        });
+    
+        return (
 
-        </div> 
-      );
+          <div>                    
+            <div>
+              <div className="ml-4 mt-3">
+                <h1 className="text-center">Estado de mi pedido</h1>
+
+                <p className="">Destino: <strong>{pedido.destino}</strong> </p>              
+                <p className="">Estado: <strong>{pedido.estado}</strong> </p>
+              </div>            
+            </div>          
+              
+              <nav className="navbar navbar-dark bg-dark">
+                <a className="navbar-brand" href="/">
+                  Productos
+                  <span className="badge badge-pill badge-light ml-2">
+                    {this.state.productos.length}
+                  </span>
+                </a>
+              </nav>
+
+              <div className="container mb-4">                     
+                <div className="col-md-8">
+                    <div className="row">
+                      {productos}
+                    </div>
+                  </div>              
+              </div>
+
+              <div>
+                <div className="ml-4">              
+                  <p className="">Observaciones: <strong>{pedido.observaciones}</strong> </p>              
+                  <p className="">Subtotal: <strong>$ {this.calcularSubtotal()}</strong> </p>
+                  <p className="">Envio: <strong>$ {pedido.envio}</strong> </p>
+
+                  <h4 className="">Total: <strong>$ {parseInt(pedido.envio) + parseInt(this.calcularSubtotal())}</strong> </h4>
+                </div>            
+              </div>
+
+          </div>         
+        );
+      }
+      else {
+        return(
+          <div>{this.state.message}</div>
+        );
+
+      }
     }
   
 }
