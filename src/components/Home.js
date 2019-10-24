98972d22a47ea8f7eefef9774324cd9e39cc21cb
Christian Alfonso <csalfonsol@unal.edu.co>
2019-10-23 23:49:43 -0500

    Prueba con Link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class Home extends Component {
     constructor() {
@@ -18,6 +19,7 @@ class Home extends Component {
               <p>
                 Esta sera la pagina principal de la aplicacion
               </p>
+              <Link to ='/mipedido'>Ir a mi pedido </Link>            
           </div>
       );
     }
